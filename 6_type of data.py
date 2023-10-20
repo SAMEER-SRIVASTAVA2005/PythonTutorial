8652d875fa4aca1af2966ef5d18f5e57ad945679
SAMEER-SRIVASTAVA2005 <sameersrivastav83033@gmail.com>
2023-10-20 21:53:55 +0530

    Day 1 code

---
+++ 6_type of data.py
@@ -0,0 +1,32 @@
+a=65
+print(a)
+b=54
+print(b)
+print(a+b)
+c=("SAM")
+print("The type of a is",type(a))
+# above line show how to find type of a variable
+print("The type of c is",type(c))
+d=1.1
+print(type(d))
+e=complex(4,5)
+print(e)
+print(type(e))
+
+list1 = [8, 2.3, [-4, 5], ["apple", "banana"]]
+print(list1) # mutable hote hai......can be changed
+
+tuple1 = (("parrot", "sparrow"), ("Lion", "Tiger"))
+print(tuple1)  # immutable hote hai......cann't be changed
+
+dict={"name":"sam","age":"45","canvote":True}
+# print(dict)
+print(type(dict))
+
+
+print(15**3) #power dene k liye do star
+print(15//9) #will give the factor till decimal place(before decimal place, not after decimal)integral division
+print(15%9) #remainder k liye
+print(22%21) #same as above
+print(63//8) #will give the factor till decimal place(before decimal place, not after decimal)integral division
+# jaise if we dividee 77 by 7 to pehle 7 into 11 se krenge then procced..........so 11 will be given off as we use // 
