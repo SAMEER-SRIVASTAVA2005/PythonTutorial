28a02cc7be1e88f2d1d1914e9234a94c83a89071
SAMEER-SRIVASTAVA2005 <sameersrivastav83033@gmail.com>
2023-10-21 12:03:09 +0530

    Day 3 code

---
+++ 7_arithmatical operator.py
@@ -0,0 +1,8 @@
+# Operator	  Operator Name	          Example
+# +	            Addition	           15+7
+# -	            Subtraction	           15-7
+# *	            Multiplication       	5*7
+# **	          Exponential	         5**3
+# /	            Division	           5/3
+# %	            Modulus	               15%7
+# //	        Floor Division          15//7  
